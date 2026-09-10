5b8b3c7d4aa58dba17afa4d6bf2d09436c4cd16e
Pedro <pedromoragues15@gmail.com>
2026-09-10 18:39:14 +0200

    feat: add basic Interval class.

--- intervals/src/main/java/com/example/math/Interval.java
+++ intervals/src/main/java/com/example/math/Interval.java
@@ -1,4 +1,32 @@
 package com.example.math;
 
-public class Interval {
+public final class Interval {
+
+    private final double start, end;
+
+    public Interval(double start, double end) {
+        if (start > end) {
+            throw new IllegalArgumentException("start > end");
+        }
+
+        this.start = start;
+        this.end = end;
+    }
+
+    public double start() {
+        return start;
+    }
+
+    public double end() {
+        return end;
+    }
+
+    public double length() {
+        return end - start;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + start + "," + end + "]";
+    }
 }
